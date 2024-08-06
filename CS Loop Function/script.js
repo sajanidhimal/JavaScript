7900b378b72d031b895535f1858402623a18e057
Sajani Dhimal <sajanidhimal1111@gmail.com>
2024-08-06 20:53:05 +0545

    DOM

---
+++ CS Loop Function/script.js
@@ -0,0 +1,23 @@
+// Task 1
+var x;
+var y;
+
+x = window.prompt("Input the first integer", "0");
+y = window.prompt("Input the second integer", "0");
+
+if (parseInt(x, 10) > parseInt(y, 10)) {
+    console.log("The larger of " + x + ' and ' + y + " is " + x + ".");
+}
+else if (parseInt(y, 10) > parseInt(x, 10)) {
+    console.log("The larger of " + x + ' and ' + y + " is " + y + ".");
+}
+else {
+    console.log("The value" + x + " and " + y + " are eual. ");
+}
+
+// Task 2
+var a = 3;
+var b = -7;
+var c = 2;
+
+if (x>0 &&)
